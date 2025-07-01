96c991dcf2fa8d5260ae2f5a5a084ce7c2871e1d
samanad <adm@perespolis.com>
2025-07-01 17:40:05 +0300

    Update upload-image.js

--- api/upload-image.js
+++ api/upload-image.js
@@ -8,6 +8,17 @@ module.exports = async (req, res) => {
 
     // Save image to server
     fs.writeFileSync(imagePath, imageData.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+
+    // Save image metadata to JSON file
+    const imageMetadataPath = path.join(process.cwd(), 'data', 'images.json');
+    const imageMetadata = JSON.parse(fs.existsSync(imageMetadataPath) ? fs.readFileSync(imageMetadataPath, 'utf-8') : '[]');
+    imageMetadata.push({
+      id: Date.now(),
+      path: imagePath,
+      uploadedAt: new Date().toISOString()
+    });
+    fs.writeFileSync(imageMetadataPath, JSON.stringify(imageMetadata, null, 2));
+
     res.status(200).json({ message: 'Image uploaded successfully' });
   } else {
     res.status(405).json({ message: 'Method not allowed' });
